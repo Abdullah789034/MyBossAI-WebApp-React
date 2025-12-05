464bf2ebfb4bd130f1c133f4197f35a2b693ec52
Muhammad Abdullah <95620935+Abdullah789034@users.noreply.github.com>
2025-12-06 04:09:33 +0500

    UpdatedAPI

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-const API_URL = 'https://api-my-boss-ai.vercel.app/api';
+const API_URL = 'https://my-boss-ai-backend.vercel.app/api';
 
 // Create axios instance
 const api = axios.create({
